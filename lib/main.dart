c3e8ae85dbacb619a6d20eb21c85ddb72c230eff
nakad-fukuoka <nakadai.2525.fu@outlook.jp>
2020-06-30 06:46:44 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,48 @@
-import 'package:craftbeer/next_page.dart';
+import 'package:craftbeer/main_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
         child: Scaffold(
           appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.map)),
-                Tab(icon: Icon(Icons.format_list_numbered)),
-                Tab(icon: Icon(Icons.pie_chart)),
-              ],
-            ),
-            title: Container(
-                child: Text('クラフトビールの世界'),
-            ),
-            actions: <Widget>[
-              Icon(Icons.add),
-              Icon(Icons.share),
-            ],
+            title: Text('クラフトビール'),
           ),
-          body: TabBarView(
-            children: [
-              Icon(Icons.map),
-              Icon(Icons.confirmation_number),
-              Icon(Icons.pie_chart),
-            ],
+          body: Consumer<MainModel>(builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      model.craftbeerText,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('ボタン'),
+                      onPressed: (){
+                        //ここで何か
+                        model.cangecraftbeerText();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         ),
       ),
     );
   }
 }
+
